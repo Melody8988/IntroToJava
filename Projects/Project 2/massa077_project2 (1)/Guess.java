46c9384f2aa43d4337cbe0384e5423d54e948e38
Melody8988 <melody.massard8988@gmail.com>
2018-10-05 13:46:32 -0500

    simple guessing game in java completed

--- Projects/Project 2/massa077_project2 (1)/Guess.java
+++ Projects/Project 2/massa077_project2 (1)/Guess.java
@@ -1,66 +1,64 @@
+// Melody Massard, massa077@umn.edu
+
 public class Guess {
 	public static void main(String[] args) { //main
+		int min, max, guess;
+		char evaluation;
 
-		int min, max, guess, newGuess; 
-		char evaluation, newEvaluation;
-
-		// your code goes here
+		//Greeting & set range of game
 		System.out.println(
-				"Hello! Let's play a game. I bet I can guess what number you are thinking. Let's see how many tries it takes.");
+				"Hello! Let's play a game. Think of a number in that brain of yours. Let's see how many tries it takes me to guess it.");
 		System.out.println(
-				"First I need you to set a range. What's the lowest your number could be? Please enter a value:");
+				"First I need some kind of range. What's the lowest your number could be?");
 
 		min = TextIO.getInt();
-		System.out.println("min set as:" + min);
+		System.out.println("minimum set as: " + min);
 
 		System.out.println("Okay great, now what's the highest your number could be?");
 		max = TextIO.getInt();
+		System.out.println("The range is: " + min + " - " + max + "\n");
 
+		//Check if min < max
 		while (max < min) {
 			System.out.println(
 					"That doesn't make sense! The upper bound must be higher than the lower bound. Please enter another number:");
 			max = TextIO.getInt();
 		}
 
+		//Initial guess & instructions to user
 		guess = Guess.randomInt(min, max);
-
-		System.out.println("Okay! I am going to guess: " + guess);
-		System.out.println("How did I do? If my guess was less than your number type <, if it was greater than your number type >, if it was equal type =");
-
+		System.out.println("Okay! My guess is: " + guess + "\n");
+		System.out.println("If my guess was \n less than your number type < \n if it was greater than your number type > \n if it was equal type =");
+		
+		//User's input to the guess
 		evaluation = TextIO.getChar();
 
-
-
+		//Continuously require an evaluation if program guess doesn't equal user's number
 		while (evaluation != '='){
 			if(evaluation == '<'){
-				guess = Guess.randomInt(guess+1, max);
+				min = guess+1;
+				guess = Guess.randomInt(min, max);
 				System.out.println("Okay, I'll guess higher");
 				System.out.println("Is your number: " + guess);
+			
 			} else if (evaluation == '>'){
-				guess = Guess.randomInt(min, guess-1);
+				max = guess-1;
+				guess = Guess.randomInt(min, max);
 				System.out.println("Okay, I'll guess lower");
 				System.out.println("Is your number: " + guess);
-
 			}
 			evaluation = TextIO.getChar();
 		}
 
+		//If user says program guess is equal to their number
 		if (evaluation == '='){
 			System.out.println("Yay! I got it right!");
 		}
-
-	
-
-		
-		
-
-
-
 	} // end of main
 
-
-
-	public static int randomInt(int min, int max) {
+	//Given randomizer
+	public static int randomInt(int min, int max) { //random method
 		return (int) ((Math.random() * ((max - min) + 1)) + min);
-	}
+	} //end of random method
+	
 } // end of Guess class
