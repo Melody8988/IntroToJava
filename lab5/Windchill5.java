117d8a6bf2121fd41bac26cb1aa0be3ed803155e
Melody8988 <melody.massard8988@gmail.com>
2018-10-08 16:46:00 -0500

    windchill iterates every 5

--- lab5/Windchill5.java
+++ lab5/Windchill5.java
@@ -1,11 +1,12 @@
 public class Windchill5 {
     public static void main(String[] args) {
-        double windchill5, windchill10, windchill15, windchill20, windchill25, tempF, iteratedTemp, lineCount;
+        double windchill, windspeed, tempF, iteratedTemp, lineCount;
         // windspeed = Double.valueOf(args[0]); //No longer taking windspeed
 
         tempF = Double.valueOf(args[0]);
         iteratedTemp = Double.valueOf(args[0]) - 20;
         lineCount = 0;
+        windchill=0;
 
         // if (windspeed < 0) {
         //     System.out.println("Error: WindSpeeds cannot be below 0. ");
@@ -15,36 +16,28 @@ public class Windchill5 {
         System.out.println("  Temp (F)     Wind=5    Wind=10    Wind=15    Wind=20    Wind=25 ");
 
         while (tempF > iteratedTemp) {
-
             if (lineCount % 5 == 0) {
-                System.out.println("-------------------------------------------------------------------\n");
-
+                System.out.println("----------------------------------------------------------------\n");
             }
+            System.out.printf("%10.1f", tempF);
+            
+        
+        for (windspeed=5; windspeed<=25; windspeed+=5 ){
+            
+            if (windspeed % 5 == 0 ){
+                windchill = (0.6215 * tempF) - (35.75 * Math.pow(windspeed, 0.16))
+                        + (0.4275 * tempF * Math.pow(windspeed, 0.16)) + 35.74;
+            
+            System.out.printf("%10.1f", windchill);
+            }
+            
+        }//end windspeed progress
 
-            // If windspeed is below 4, the windchill is just air temperature
-            // if (windspeed < 4) {
-            //     windchill = tempF;
-            // } //No longer accepting windchill
-       
-
-            windchill5 = (0.6215 * tempF) - (35.75 * Math.pow(5, 0.16))
-                        + (0.4275 * tempF * Math.pow(5, 0.16)) + 35.74;
-
-            windchill10 = (0.6215 * tempF) - (35.75 * Math.pow(10, 0.16))
-                        + (0.4275 * tempF * Math.pow(10, 0.16)) + 35.74;
-
-            windchill15 = (0.6215 * tempF) - (35.75 * Math.pow(15, 0.16))
-                        + (0.4275 * tempF * Math.pow(15, 0.16)) + 35.74;
-
-            windchill20 = (0.6215 * tempF) - (35.75 * Math.pow(20, 0.16))
-                        + (0.4275 * tempF * Math.pow(20, 0.16)) + 35.74;
-
-            windchill25 = (0.6215 * tempF) - (35.75 * Math.pow(25, 0.16))
-                        + (0.4275 * tempF * Math.pow(25, 0.16)) + 35.74;
-    
-            System.out.printf("%10.1f %10.1f %10.1f %10.1f %10.1f %10.1f\n", tempF, windchill5, windchill10, windchill15, windchill20, windchill25 );
+        System.out.printf("\n");
+            
             tempF--;
             lineCount++;
-        }
-    }
+        }//end linecounting
+
+    }//main
 }
