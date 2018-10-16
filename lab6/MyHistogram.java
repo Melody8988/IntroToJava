6c3a6e0533b6db44e14670b41c52dcad0852f576
Melody8988 <melody.massard8988@gmail.com>
2018-10-15 23:20:06 -0500

    lab6

---
+++ lab6/MyHistogram.java
@@ -0,0 +1,55 @@
+// Write a loop (and locate it between your initialization and printing loops in your code from Step 1 above)
+// that will process each legal score from args by updating the appropriate index in hist. Compile and run
+// your program. With values entered into your histogram, you should now see some output. When you are
+// convinced that your code is working, have a TA check this step.
+
+public class MyHistogram {
+    public static void main(String[] args) {
+        int[] hist = new int[101];
+        int i = 0;
+
+        // Initialize all score fruequencies to 0
+        for (i = 0; i < 100; i++) {
+            hist[i] = 0;
+        }
+
+        // Replace 0 hist values to inputs
+        for (i = 0; i < args.length; i++) {
+            int n = Integer.valueOf(args[i]);
+            if (n >= 0 && n < 100) {
+                hist[n] += 1;
+            }
+        }
+
+        // Print non zero indexes
+        for (i = 0; i < hist.length; i++) {
+            if (hist[i] != 0) {
+                System.out.println("Non zero numbers in histogram: " + i);
+            }
+
+        }
+
+        //Find lowest value
+        for (i = 0; i < hist.length; i++) {
+            int min = Integer.valueOf(args[0]);
+            if (Integer.valueOf(args[i]) < min) {
+                min = Integer.valueOf(args[i]);
+                System.out.println("Lowest value is: " + min);
+            } 
+        }
+
+        // //Find highest value
+        // for (i = 0; i < hist.length; i++) {
+        //     int max = Integer.valueOf(args[0]);
+        //     if (Integer.valueOf(args[i]) > max) {
+        //         max = Integer.valueOf(args[i]);
+        //         System.out.println("Highest value is: " + max);
+        //     } 
+        // }
+       
+
+     
+
+
+    }
+}
