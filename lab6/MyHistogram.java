b30f75915816d0f134eb35f4f8c168bbac9fd667
Melody8988 <melody.massard8988@gmail.com>
2018-10-17 09:04:44 -0500

    mean and mode

--- lab6/MyHistogram.java
+++ lab6/MyHistogram.java
@@ -7,6 +7,7 @@ public class MyHistogram {
     public static void main(String[] args) {
         int[] hist = new int[101];
         int i = 0;
+    
 
         // Initialize all score fruequencies to 0
         for (i = 0; i < 100; i++) {
@@ -22,30 +23,44 @@ public class MyHistogram {
         }
 
         // Print non zero indexes
+        int maxFrequency = Integer.valueOf(hist[0]);
         for (i = 0; i < hist.length; i++) {
             if (hist[i] != 0) {
                 System.out.println("Non zero numbers in histogram: " + i);
+            }
+            if (hist[i] > maxFrequency){
+                
             }
 
         }
 
         //Find lowest value
-        for (i = 0; i < hist.length; i++) {
-            int min = Integer.valueOf(args[0]);
+        int min = Integer.valueOf(args[0]);
+        int max = Integer.valueOf(args[0]);
+        int mean = 0;
+        int mode;
+        for (i = 0; i < args.length; i++) {
+            
             if (Integer.valueOf(args[i]) < min) {
                 min = Integer.valueOf(args[i]);
-                System.out.println("Lowest value is: " + min);
             } 
-        }
+            if (Integer.valueOf(args[i]) > max) {
+                max = Integer.valueOf(args[i]);
+            } 
+            
+            mean = mean + Integer.valueOf(args[i]);
+            
 
-        // //Find highest value
-        // for (i = 0; i < hist.length; i++) {
-        //     int max = Integer.valueOf(args[0]);
-        //     if (Integer.valueOf(args[i]) > max) {
-        //         max = Integer.valueOf(args[i]);
-        //         System.out.println("Highest value is: " + max);
-        //     } 
-        // }
+        }
+        System.out.println("Lowest value is: " + min);
+        System.out.println("Highest value is: " + max);
+        System.out.println("Mean is: " + mean);
+        // System.out.println("Mode is: " + mode);
+        // System.out.println("Median is: " + median);
+
+         
+
+        
        
 
      
